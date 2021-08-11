2b004a3b8bafefd817f2f50cf999f3a9352bc033
Jennifer Switzer <jfi.switzer@gmail.com>
2021-08-10 21:30:30 -0700

    fix

--- cl2.sh
+++ cl2.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
-powerstat -z -d 0 0.5 960 >> 2_pstat$1.out &
+powerstat -z -d 0 0.5 960 >> 20_pstat2.out &
 sleep 60;
-yes > /dev/null & sudo cpulimit -p $! --limit $1 &
-yes > /dev/null & sudo cpulimit -p $! --limit $1 & sleep 360;
+yes > /dev/null & sudo cpulimit -p $! --limit 20 &
+yes > /dev/null & sudo cpulimit -p $! --limit 20 & sleep 360;
 pkill yes;
-sleep 60
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 40_pstat2.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 40 &
+yes > /dev/null & sudo cpulimit -p $! --limit 40 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 60_pstat2.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 60 &
+yes > /dev/null & sudo cpulimit -p $! --limit 60 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 80_pstat2.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 80 &
+yes > /dev/null & sudo cpulimit -p $! --limit 80 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 100_pstat2.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 100 &
+yes > /dev/null & sudo cpulimit -p $! --limit 100 & sleep 360;
+pkill yes;
+sleep 60;
--- cl4.sh
+++ cl4.sh
@@ -1,9 +1,45 @@
 #!/bin/bash
-powerstat -z -d 0 0.5 960 >> 4_pstat$1.out &
+powerstat -z -d 0 0.5 960 >> 20_pstat4.out &
 sleep 60;
-yes > /dev/null & sudo cpulimit -p $! --limit $1 &
-yes > /dev/null & sudo cpulimit -p $! --limit $1 &
-yes > /dev/null & sudo cpulimit -p $! --limit $1 &
-yes > /dev/null & sudo cpulimit -p $! --limit $1 & sleep 360;
+yes > /dev/null & sudo cpulimit -p $! --limit 20 &
+yes > /dev/null & sudo cpulimit -p $! --limit 20 &
+yes > /dev/null & sudo cpulimit -p $! --limit 20 &
+yes > /dev/null & sudo cpulimit -p $! --limit 20 & sleep 360;
 pkill yes;
-sleep 60
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 40_pstat4.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 40 &
+yes > /dev/null & sudo cpulimit -p $! --limit 40 &
+yes > /dev/null & sudo cpulimit -p $! --limit 40 &
+yes > /dev/null & sudo cpulimit -p $! --limit 40 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 60_pstat4.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 60 &
+yes > /dev/null & sudo cpulimit -p $! --limit 60 &
+yes > /dev/null & sudo cpulimit -p $! --limit 60 &
+yes > /dev/null & sudo cpulimit -p $! --limit 60 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 80_pstat4.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 80 &
+yes > /dev/null & sudo cpulimit -p $! --limit 80 &
+yes > /dev/null & sudo cpulimit -p $! --limit 80 &
+yes > /dev/null & sudo cpulimit -p $! --limit 80 & sleep 360;
+pkill yes;
+sleep 60;
+
+powerstat -z -d 0 0.5 960 >> 100_pstat4.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit 100 &
+yes > /dev/null & sudo cpulimit -p $! --limit 100 &
+yes > /dev/null & sudo cpulimit -p $! --limit 100 &
+yes > /dev/null & sudo cpulimit -p $! --limit 100 & sleep 360;
+pkill yes;
+sleep 60;
