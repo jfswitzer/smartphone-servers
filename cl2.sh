f3e6b99adeafcd477975fca3d846ccdd383896d3
Jennifer Switzer <jfi.switzer@gmail.com>
2021-08-10 19:51:47 -0700

    more cpu

---
+++ cl2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+powerstat -z -d 0 0.5 960 >> 2_pstat$1.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit $1 &
+yes > /dev/null & sudo cpulimit -p $! --limit $1 & sleep 360;
+pkill yes;
+sleep 60
---
+++ cl3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+powerstat -z -d 0 0.5 960 >> 3_pstat$1.out &
+sleep 60;
+yes > /dev/null & sudo cpulimit -p $! --limit $1 &
+yes > /dev/null & sudo cpulimit -p $! --limit $1 &
+yes > /dev/null & sudo cpulimit -p $! --limit $1 & sleep 360;
+pkill yes;
+sleep 60
